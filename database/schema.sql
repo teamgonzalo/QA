c2d8779e0de4f5bd4dde6893d54734f4367ace79
Thomas Rose <thomascrose90@gmail.com>
2021-11-17 16:00:42 -0500

    Update date_written type to varchar to avoid loading issues. Will parse formatting on server side.

--- database/schema.sql
+++ database/schema.sql
@@ -2,19 +2,19 @@ CREATE TABLE IF NOT EXISTS questions (
   id SERIAL,
   product_id INTEGER NOT NULL,
   body TEXT NOT NULL,
-  date_written TIMESTAMP NOT NULL,
+  date_written VARCHAR(20) NOT NULL,
   asker_name VARCHAR(50) NOT NULL,
   asker_email VARCHAR(50),
   reported INTEGER,
   helpful INTEGER,
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS answers (
   id SERIAL,
   question_id INTEGER NOT NULL,
   body TEXT NOT NULL,
-  date_written TIMESTAMP NOT NULL,
+  date_written VARCHAR(20) NOT NULL,
   answerer_name VARCHAR(50) NOT NULL,
   answerer_email VARCHAR(50),
   reported INTEGER,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS answers (
   PRIMARY KEY (id),
   FOREIGN KEY (question_id)
     REFERENCES questions(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS photos (
   id SERIAL,
@@ -31,19 +31,19 @@ CREATE TABLE IF NOT EXISTS photos (
   PRIMARY KEY (id),
   FOREIGN KEY (answer_id)
     REFERENCES answers(id)
-)
+);
 
 COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM './data/questions.csv'
+FROM '/Users/thomasrose/Hack Reactor/HRRFE6/SDC/QA/database/data/questions.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM './data/answers.csv'
+FROM '/Users/thomasrose/Hack Reactor/HRRFE6/SDC/QA/database/data/answers.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY photos(id, answer_id, body)
-FROM './data/answers_photos.csv'
+FROM '/Users/thomasrose/Hack Reactor/HRRFE6/SDC/QA/database/data/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
