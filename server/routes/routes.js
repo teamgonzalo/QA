cb2b27c6581816f410e6f34c27f5c3ba34fc2a68
Thomas Rose <thomascrose90@gmail.com>
2021-11-18 15:57:16 -0500

    Create router method calls with specific endpoints. Function calls to be built now.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-router.get('/questions', (req, res) => {
-
-})
+// Pass controller functions in as they are built
+router.get('/questions', );
+router.post('/questions', );
+router.get('/questions/:question_id/answers', );
+router.post('/questions/:question_id/answers', );
+router.put('/questions/:question_id/helpful', );
+router.put('/questions/:question_id/report', );
+router.put('/answers/:answer_id/helpful', );
+router.put('/answers/:answer_id/report', );
 
 module.exports = router;
